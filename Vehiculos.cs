00bbe832ac4699ae6577ab22263e672fff15cd81
6 <104390006+claystiv@users.noreply.github.com>
2022-06-01 11:15:47 -0500

    Update Vehiculos.cs

--- Vehiculos.cs
+++ Vehiculos.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,53 +9,90 @@ namespace Diagrama_de_clases
     internal class Vehiculos
     {
 
+        private string Nombre;
+        private string Marca;
+        private string Color;
+        private double Alto;
+        private double Largo;
+        private double Ancho;
+        private string Modelo;
+        private char Uso;
+        private double Velocidad;
+        private int Asientos;
 
-        public Vehiculos()
+        public Vehiculos(string Nombre, string Marca, string Color, double Alto, double Largo, double Ancho, string Modelo, char Uso, double Velocidad,int Asientos)
         {
-            NombreVehiculo = "Carro";
-            ColorVehiculo = "Rojo";
-            AltoVehiculo = 1.517;
-            AnchoVehiculo = 1.735;
-            LargoVehiculo = 4.399;
-            ModeloVehiculo = "SUV";
-            CapacidadVehiculo = 5;
-            UsoVehiculo = '0';
-            VelocidadVehiculo = 0;
-            AsientosVehiculos = 5;
-            //10
-        }
-        private string NombreVehiculo;
-        private string ColorVehiculo;
-        private double AltoVehiculo;
-        private double LargoVehiculo;
-        private double AnchoVehiculo;
-        private string ModeloVehiculo;
-        private int CapacidadVehiculo;
-        private char UsoVehiculo;
-        private double VelocidadVehiculo;
-        private int AsientosVehiculos;
 
-        public void setInfoVehiculo(double AltoVehiculo, double LargoVehiculo, double AnchoVehiculo, double VelocidadVehiculo, string NombreVehiculo, string ColorVehiculo, string ModeloVehiculo, int CapacidadVehiculo, int AsientosVehiculo, char UsoVehiculo)
-        {
-            this.AltoVehiculo = AltoVehiculo;
-            this.LargoVehiculo = LargoVehiculo;
-            this.AnchoVehiculo = AnchoVehiculo;
-            this.VelocidadVehiculo = VelocidadVehiculo;
-            this.NombreVehiculo = NombreVehiculo;
-            this.CapacidadVehiculo = CapacidadVehiculo;
-            this.ModeloVehiculo = ModeloVehiculo;
-            this.AsientosVehiculos = AsientosVehiculo;
-            this.UsoVehiculo = UsoVehiculo;
-            this.ColorVehiculo = ColorVehiculo;
-        }
-        public string getInfoVehiculo()
-        {
-            return "El vehiculo tiene la siguiente informacion: \n El alto del vehiculo: " + AltoVehiculo + " \n El largo del vehiculo: " + LargoVehiculo +
-                  " \n El ancho del vehiculo: " + AnchoVehiculo + " \n La velocidad del velocidad: " + VelocidadVehiculo + "\n El nombre del vehiculo: " + NombreVehiculo +
-                  "\n El color del vehiculo es: " + ColorVehiculo + "\n El modelo del vehiculo: " + ModeloVehiculo + "\n La capacidad del vehiculo es: " + CapacidadVehiculo +
-                  "\n El uso del vehiculo es: " + UsoVehiculo + "\n Los asientos del vehiculo: " + AsientosVehiculos;
+            this.Nombre = Nombre;
+            this.Marca = Marca;
+            this.Color = Color;
+            this.Alto = Alto;
+            this.Largo = Largo;
+            this.Ancho = Ancho;
+            this.Modelo = Modelo;
+            this.Uso = Uso;
+            this.Velocidad = Velocidad;
+            this.Asientos = Asientos;
+
+
         }
 
+
+        public String GetNombre()
+        {return Nombre;}
+
+        public void SetNombre(string Nombre)
+        {this.Nombre = Nombre;}
+
+        public String GetMarca()
+            { return Marca; }
+
+        public void SetMarca(string Marca)
+        {this.Marca = Marca;}
+
+        public String GetColor()
+        { return Color; }
+
+        public void SetColor(string Color)
+        { this.Color = Color; }
+
+        public double GetAlto()
+        { return Alto; }
+
+        public void SetAlto(double Alto)
+        { this.Alto = Alto; }
+
+
+        public double GetLargo()
+        { return Largo; }
+
+        public void SetLargo(double Largo)
+        { this.Largo = Largo; }
+
+
+        public String GetModelo()
+        { return Modelo; }
+
+        public void SetModelo(string Modelo)
+        { this.Modelo = Modelo; }
+
+        public char GetUso()
+        { return Uso; }
+
+        public void SetUso(char Uso)
+        { this.Uso = Uso; }
+
+        public double GetVelocidad()
+        { return Velocidad; }
+
+        public void SetVelocidad(double Velocidad)
+        { this.Velocidad = Velocidad; }
+
+        public int GetAsientos()
+        { return Asientos; }
+
+        public void SetAsientos(int Asientos)
+        { this.Asientos = Asientos; }
 
         public bool Transportar()
         {
@@ -72,4 +109,5 @@ namespace Diagrama_de_clases
     }
 
 }
+
 
