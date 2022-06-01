61290671ba3b8bea07ed361c9393c6a636666ba4
6 <104390006+claystiv@users.noreply.github.com>
2022-06-01 11:17:47 -0500

    Update Camionetas.cs

--- Camionetas.cs
+++ Camionetas.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,54 +6,40 @@ using System.Threading.Tasks;
 
 namespace Diagrama_de_clases
 {
-    internal class Camionetas
+    internal class Camionetas 
     {
-        public Camionetas()
-        {
-            techo = true;
-            puertas = 3;
-            ventanas = 2;
-            parachoques = true;
-            clindraje = 10;
-            remolqueCarga = true;
-            DepositoAceite = 1;
-        }
-        private bool techo;
-        private int puertas;
-        private int ventanas;
-        private bool parachoques;
-        private int clindraje;
-        private bool remolqueCarga;
-        private int DepositoAceite;
+        private string Parachoques;
+        private int Cilindraje;
+        private string Remolque;
 
-        public void setCamiiones(bool techo, int puertas, int ventanas, bool parachoques, int clindraje, bool remolqueCarga, int DepositoAceite)
+        public Camionetas(string Parachoques, int Cilindraje,string Remolque)
         {
-            this.techo = techo;
-            this.puertas = puertas;
-            this.ventanas = ventanas;
-            this.parachoques = parachoques;
-            this.clindraje = clindraje;
-            this.remolqueCarga = remolqueCarga;
-            this.DepositoAceite = DepositoAceite;
-        }
-        public string getCamiones()
-        {
-            return "El vehiculo tiene la siguiente informacion: \n Si tiene techo: "
-              + techo + "\n El numero de puertas: " + puertas +
-              "\n El numero de ventanas: " + ventanas + "\n Si tiene parachoques: " + parachoques + "\n El cilindraje: "
-              + clindraje + "\n Si tiene remolque de carga: " + remolqueCarga + "\n El deposito de aceite: " + DepositoAceite;
+            this.Parachoques = Parachoques;
+            this.Cilindraje = Cilindraje;
+            this.Remolque = Remolque;
 
         }
 
-        public bool cargan()
-        {
-            Console.WriteLine("Los camiones cargan");
-            return true;
-        }
-        public bool Enciendenmotor()
-        {
-            Console.WriteLine("El camion enciende el motor");
-            return true;
-        }
+
+        public string GetParachoque()
+        { return Parachoques; }
+
+        public void SetParachoque(string Parachoque)
+        { this.Parachoques = Parachoque; }
+
+        public int GetCilindraje()
+        { return Cilindraje; }
+
+        public void SetCilindraje(int Cilindraje)
+        { this.Cilindraje = Cilindraje; }
+
+        public string GetRemolque()
+        { return Remolque; }
+
+        public void SetRemolque(string Remolque)
+        { this.Remolque = Remolque; }
+
+
+
     }
 }
